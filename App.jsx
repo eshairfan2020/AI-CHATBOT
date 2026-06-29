b2c78045e068a56d41f36706d010b9f88c5388cf
eshairfan2020 <eeshairfan2020@gmail.com>
2026-06-29 13:42:45 +0500

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChatBot from "./components/ChatBot";
+
+function App() {
+  return (
+    <div className="app">
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App;
